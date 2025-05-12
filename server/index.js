706078c09c40e57fb4e2a30d7730e02f3ccd2a9f
ajaykumar2505 <ajaykumarbadineni2505@gmail.com>
2025-05-13 02:34:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ require('dotenv').config();
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 app.use(express.static(path.join(__dirname, '../client'))); // Serve static files
+app.use(cors({
+  origin: 'https://gym-1-3374.onrender.com', // your frontend domain
+  credentials: true, // if you use cookies or authorization headers
+}));
 
 // Database connection
 MongoDB();
