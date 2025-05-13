e955cf6cd44bf25b20c12bfeda5b7a96845bf29c
ajaykumar2505 <ajaykumarbadineni2505@gmail.com>
2025-05-13 13:25:06 +0530

    Update userrouter.js

--- server/routes/userrouter.js
+++ server/routes/userrouter.js
@@ -14,7 +14,7 @@ router.post('/userdetailsinsertion', async (req, res) => {
         return res.status(200).json({
             message: "Successfully Inserted",
             success: true
-
+            data:data
         })
     } catch (error) {
         return res.status(500).json({
